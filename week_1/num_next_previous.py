d429b635b26de67e749752eb03cf8796f256f2c4
Searge <ashikcygan@gmail.com>
2019-05-05 17:38:17 +0300

    Next & precious :1234:

---
+++ week_1/num_next_previous.py
@@ -0,0 +1,9 @@
+# ALT:
+# https://github.com/Searge/PythonBasics_HSEU/blob/master/week_1/num_n_odd.py
+n = int(input())
+
+next_ = n + 1
+prev_ = n - 1
+
+print(f"The next number for the number {n} is {next_}.")
+print(f"The previous number for the number {n} is {prev_}.")
